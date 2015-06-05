31c683e19cc1c43ff363e366d57b7e897251bdda
Rajesh.Sah <rajesh.sah@urbanladder.com>
2015-06-05 12:36:05 +0530

    overided + and * and wrote test for them

--- lib/length.rb
+++ lib/length.rb
@@ -1,17 +1,36 @@
-#it represents lenght in mm
+#This represents one dimensional length in matric unit
 class Length
   UNIT_MILLIMETER = "mm"
   UNIT_METER = "m"
   UNIT_CENTIMETER = "cm"
   attr_reader :value, :unit
+  alias_method :eql?, :==
+
   def initialize(value, unit)
     @unit = unit
-    @value = value * 1000 if unit == UNIT_METER
-    @value = value * 10 if unit ==  UNIT_CENTIMETER
-    @value = value  if unit == UNIT_MILLIMETER
+    @value = value
   end
 
   def ==(other_length)
-    (self.value == other_length.value)
+    (convert_to_millimeter(self) == convert_to_millimeter(other_length))
   end
+
+  def +(other_length)
+    Length.new((convert_to_millimeter(self) + convert_to_millimeter(other_length)), UNIT_MILLIMETER)
+  end
+
+  def *(number)
+    Length.new((convert_to_millimeter(self) * number), UNIT_MILLIMETER)
+  end
+
+  def hash
+    value.hash
+  end
+
+  private
+    def convert_to_millimeter(length)
+      return value_in_mm = length.value * 1000 if length.unit == UNIT_METER
+      return value_in_mm = length.value * 10 if length.unit ==  UNIT_CENTIMETER
+      return value_in_mm = length.value  if length.unit == UNIT_MILLIMETER
+    end
 end
--- spec/length_spec.rb
+++ spec/length_spec.rb
@@ -3,19 +3,67 @@ require 'spec_helper'
 describe Length do
   context "Equality" do
     it "should be equal to itself" do
-      expect(Length.new(10, Length::UNIT_MILLIMETER)).to eq(Length.new(10, Length::UNIT_MILLIMETER))
+      length = Length.new(10, Length::UNIT_MILLIMETER)
+      expect(length).to eq(length)
     end
 
-    it "should not be equal to other" do
+    it "with 10 mm should not be equal to length with 11 mm" do
       expect(Length.new(10, Length::UNIT_MILLIMETER)).to_not eq(Length.new(11, Length::UNIT_MILLIMETER))
     end
 
-    it "with 10 cm should return 100" do
+    it "with 10 cm should be equal to 100 mm" do
       expect(Length.new(10, Length::UNIT_CENTIMETER)).to eq(Length.new(100, Length::UNIT_MILLIMETER))
     end
 
-    it "with 10.1 m should return 1010" do
+    it "with 10.1 m should be equal to 1010 cm" do
       expect(Length.new(10.1, Length::UNIT_METER)).to eq(Length.new(1010, Length::UNIT_CENTIMETER))
+    end
+  end
+
+  context "Add" do
+    it "should be equal same when lenght of 0 m is added" do
+      length = Length.new(10, Length::UNIT_MILLIMETER)
+      expect(length + Length.new(0, Length::UNIT_METER)).to eq(length)
+    end
+
+    it "with 10 mm should return length 15 mm when length with 5mm is added" do
+      length1 = Length.new(10, Length::UNIT_MILLIMETER)
+      length2 = Length.new(5, Length::UNIT_MILLIMETER)
+      length3 = Length.new(15, Length::UNIT_MILLIMETER)
+      expect(length1 + length2).to eq(length3)
+    end
+
+    it "with 10 mm should return length 15 mm when length with 5mm is added" do
+      length1 = Length.new(10.1, Length::UNIT_METER)
+      length2 = Length.new(5, Length::UNIT_CENTIMETER)
+      length3 = Length.new(10.15, Length::UNIT_METER)
+      expect(length1 + length2).to eq(length3)
+    end
+
+    it "with 10 mm should return length 15 mm when length with 5mm is added" do
+      length1 = Length.new(10, Length::UNIT_METER)
+      length2 = Length.new(5, Length::UNIT_MILLIMETER)
+      length3 = Length.new(1000.5, Length::UNIT_CENTIMETER)
+      expect(length1 + length2).to eq(length3)
+    end
+  end
+
+  context "Multiplication" do
+    it "should be equal same when integer 1 is multiplied" do
+      length = Length.new(10, Length::UNIT_MILLIMETER)
+      expect(length * 1).to eq(length)
+    end
+
+    it "with 10m should be equal to length with 20m when integer 2 is multiplied" do
+      length1 = Length.new(10, Length::UNIT_METER)
+      length2 = Length.new(20, Length::UNIT_METER)
+      expect(length1 * 2).to eq(length2)
+    end
+
+    it "with 100cm should be equal to length with 5m when integer 5 is multiplied" do
+      length1 = Length.new(100, Length::UNIT_CENTIMETER)
+      length2 = Length.new(5, Length::UNIT_METER)
+      expect(length1 * 5).to eq(length2)
     end
   end
 end
